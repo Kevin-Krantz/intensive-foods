301724e55d5ba4b14acdc921e6d33fcfbebaf440
Kevin-Krantz <kevinkrantz95@hotmail.com>
2022-07-27 21:50:14 +0200

    Implemented Pagination component

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { getFoods } from "../services/fakeFoodService";
 import Favorite from "./common/Favorite";
+import Pagination from "./common/Pagination";
 
 class Foods extends Component {
   state = {
     foods: getFoods(),
+    pageSize: 4,
+    selectedPage: 1,
   };
 
   handleFavor = (food) => {
@@ -15,52 +18,66 @@ class Foods extends Component {
     this.setState({ foods });
   };
 
+  handlePageChange = (page) => {
+    this.setState({ selectedPage: page });
+  };
+
   render() {
     const { length: count } = this.state.foods;
+    const { pageSize, selectedPage } = this.state;
 
     if (count === 0) return <p>There are no foods in the database</p>;
 
     return (
-      <>
-        {<p className="badge bg-dark">hej</p>}
-        <p>Showing {count} foods in the database</p>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Category</th>
-              <th>Stock</th>
-              <th>Price</th>
-              <th />
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.foods.map((food) => (
-              <tr key={food._id}>
-                <td>{food.name}</td>
-                <td>{food.category.name}</td>
-                <td>{food.numberInStock}</td>
-                <td>{food.price}</td>
-                <td>
-                  <Favorite
-                    onFavor={() => this.handleFavor(food)}
-                    isFavorite={food.isFavorite}
-                  />
-                </td>
-                <td>
-                  <button
-                    onClick={() => this.handleDetelete(food)}
-                    className="btn btn-danger"
-                  >
-                    Delete
-                  </button>
-                </td>
+      <div className="row">
+        <div className="col-2"></div>
+        <div className="col">
+          <p>Showing {count} foods in the database</p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Stock</th>
+                <th>Price</th>
+                <th />
+                <th />
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </>
+            </thead>
+            <tbody>
+              {this.state.foods.map((food) => (
+                <tr key={food._id}>
+                  <td>{food.name}</td>
+                  <td>{food.category.name}</td>
+                  <td>{food.numberInStock}</td>
+                  <td>{food.price}</td>
+                  <td>
+                    <Favorite
+                      onFavor={() => this.handleFavor(food)}
+                      isFavorite={food.isFavorite}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => this.handleDetelete(food)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Pagination
+            itemCount={count}
+            pageSize={pageSize}
+            selectedPage={selectedPage}
+            onPageChange={this.handlePageChange}
+          />
+        </div>
+        {<p className="badge bg-dark">hej</p>}
+      </div>
     );
   }
 
---
+++ src/components/common/Pagination.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import _ from "lodash";
+
+function Pagination({ itemCount, pageSize, selectedPage, onPageChange }) {
+  const pageCount = Math.ceil(itemCount / pageSize);
+
+  if (pageCount === 1) return null;
+
+  const pages = _.range(1, pageCount + 1);
+
+  return (
+    <ul className="pagination">
+      {pages.map((page) => (
+        <li
+          key={page}
+          className={page === selectedPage ? "page-item active" : "page-item"}
+        >
+          <button onClick={() => onPageChange(page)} className="page-link">
+            {page}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Pagination;
