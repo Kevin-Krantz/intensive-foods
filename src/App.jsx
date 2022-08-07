d515fa10d633fe65fcda53dc70ab3738d8c57abd
Kevin-Krantz <kevinkrantz95@hotmail.com>
2022-08-07 02:55:41 +0200

    Implemented LoginForm with inputs in LoginForm.jsx and installed query-string, react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Foods from "./components/Foods";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
   render() {
     return (
       <div className="container">
-        <Foods />
+        <Switch>
+          <Route path="/login" component={LoginForm} />
+          <Route path="/foods" component={Foods} />
+          <Redirect to="/foods" />
+        </Switch>
       </div>
     );
   }
